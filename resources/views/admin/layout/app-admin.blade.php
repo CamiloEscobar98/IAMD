ca3fb0968c7d951e5d4480646351047a7d0c2542
Andrés Yáñez <andrescamiloye@ufps.edu.co>
2022-07-16 17:13:07 -0500

    Creating some admin views, login and RouteServiceProvider configuration.

---
+++ resources/views/admin/layout/app-admin.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ asset('adminlte/plugins/fontawesome-free/css/all.min.css') }}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ asset('adminlte/dist/css/adminlte.min.css') }}">
+
+    @yield('css')
+
+    @yield('custom_css')
+</head>
+
+<body class="hold-transition sidebar-mini">
+    <div class="wrapper">
+        @include('admin.partials.navbar')
+
+        @include('admin.partials.sidebar')
+    </div>
+
+
+    @yield('js')
+
+    @yield('custom_js')
+</body>
+
+</html>
