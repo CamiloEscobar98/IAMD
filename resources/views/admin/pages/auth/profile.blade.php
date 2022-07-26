00815ab7f5f1c8689d6fd64941b65e1aed968d31
Andrés Yáñez <andrescamiloye@ufps.edu.co>
2022-07-26 09:11:35 -0500

    Profile text title and subtitle fixed

--- resources/views/admin/pages/auth/profile.blade.php
+++ resources/views/admin/pages/auth/profile.blade.php
@@ -1,19 +1,19 @@
 @extends('admin.layout.app')
 
-@section('title', __('pages.profile.title'))
+@section('title', __('admin_pages.profile.title'))
 
 @section('content-header')
     <section class="content-header">
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>{{ __('pages.profile.subtitle') }}</h1>
+                    <h1>{{ __('admin_pages.profile.subtitle') }}</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="{{ route('admin.home') }}">{{ __('pages.home.title') }}</a>
+                        <li class="breadcrumb-item"><a href="{{ route('admin.home') }}">{{ __('admin_pages.home.title') }}</a>
                         </li>
-                        <li class="breadcrumb-item active">{{ __('pages.profile.title') }}</li>
+                        <li class="breadcrumb-item active">{{ __('admin_pages.profile.title') }}</li>
                     </ol>
                 </div>
             </div>
