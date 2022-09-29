4f9ea5d11dac2ebf6d72dc900a74a93f0490cb51
Andrés Yáñez <andrescamiloye@ufps.edu.co>
2022-09-28 23:47:58 -0500

    Creator External Form fixed

--- app/Http/ViewComposers/Client/Creators/External/CreateCreatorExternalComposer.php
+++ app/Http/ViewComposers/Client/Creators/External/CreateCreatorExternalComposer.php
@@ -6,6 +6,8 @@ use Illuminate\View\View;
 
 use App\Repositories\Admin\ExternalOrganizationRepository;
 use App\Repositories\Admin\AssignmentContractRepository;
+use App\Repositories\Admin\CityRepository;
+use App\Repositories\Admin\CountryRepository;
 use App\Repositories\Admin\DocumentTypeRepository;
 use App\Repositories\Admin\StateRepository;
 
@@ -20,6 +22,12 @@ class CreateCreatorExternalComposer
     /** @var DocumentTypeRepository */
     protected $documentTypeRepository;
 
+    /** @var CountryRepository */
+    protected $countryRepository;
+
+    /** @var CityRepository */
+    protected $cityRepository;
+
     /** @var StateRepository */
     protected $stateRepository;
 
@@ -27,24 +35,60 @@ class CreateCreatorExternalComposer
         ExternalOrganizationRepository $externalOrganizationRepository,
         AssignmentContractRepository $assignmentContractRepository,
         DocumentTypeRepository $documentTypeRepository,
+
+        CountryRepository $countryRepository,
         StateRepository $stateRepository,
+        CityRepository $cityRepository
     ) {
         $this->externalOrganizationRepository = $externalOrganizationRepository;
         $this->assignmentContractRepository = $assignmentContractRepository;
         $this->documentTypeRepository = $documentTypeRepository;
+
+        $this->countryRepository = $countryRepository;
         $this->stateRepository = $stateRepository;
+        $this->cityRepository = $cityRepository;
     }
 
     public function compose(View $view)
     {
+
+        $countries = $this->countryRepository->all();
+
+        $country = $countries->where('id', old('country_id', 11))->first();
+
+        $states = $this->stateRepository->getByCountry($country);
+
+        if ($states->count() > 0) {
+            $state = old('state_id') ? $states->where('id', old('state_id'))->first() : $states->first();
+            $cities = $this->cityRepository->getByState($state);
+            $city = old('expedition_place_id') ? $cities->where('id', old('expedition_place_id'))->first() : $cities->first();
+        } else {
+            $states = [];
+            $cities = [];
+            $state = $this->stateRepository->newInstance();
+            $city = $this->cityRepository->newInstance();
+        }
+
+
         $externalOrganizations = $this->externalOrganizationRepository->all();
 
         $assignmentContracts = $this->assignmentContractRepository->all()->where('is_internal', false);
 
         $documentTypes = $this->documentTypeRepository->all();
 
-        $states = $this->stateRepository->search([], ['country', 'cities'])->get();
 
-        $view->with(compact('externalOrganizations', 'assignmentContracts', 'documentTypes', 'states'));
+        $view->with(compact(
+            'externalOrganizations',
+            'assignmentContracts',
+            'documentTypes',
+
+            'countries',
+            'states',
+            'cities',
+
+            'country',
+            'state',
+            'city'
+        ));
     }
 }
