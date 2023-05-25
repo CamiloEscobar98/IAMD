f795bb868b58edf4bef6010a25910513c5cfd8e4
Andrés Yáñez <andrescamiloye@ufps.edu.co>
2023-05-25 11:02:16 -0500

    NN

--- resources/views/client/emails/users/reset_password.blade.php
+++ resources/views/client/emails/users/reset_password.blade.php
@@ -3,4 +3,8 @@
 <p>Buenos días estimado usuario {{ $user->name }}.</p>
 
 <p>La restauración de su contraseña ha sido todo un éxito. Le informamos que su nueva contraseña es
-    <b>{{ $password }}</b></p>
+    <b>{{ $password }}</b>
+</p>
+
+<p>Por favor es importante que después de una restauración de contraseña, tengan la disposición de cambiarla a
+    preferencia.</p>
